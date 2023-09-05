f7473dcd7a839fa28795bd306bc15cb5e47eed75
Gavin Kemp <gavin@sungod.co>
2023-09-05 15:24:12 +0100

    Nest page folder

--- pages/support/index.vue
+++ pages/support/index.vue
@@ -1,6 +1,11 @@
 <template>
 <div class="support">Page content (should be purple)</div>
 </template>
+<script>
+export default {
+    name: 'SupportPage'
+}
+</script>
 <style lang="postcss">
     .support {
         color: purple;
