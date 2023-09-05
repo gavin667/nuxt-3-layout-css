503ee556afc102c575ce7a064c5c8ffaf5c53c9b
Gavin Kemp <gavin@sungod.co>
2023-09-05 15:12:51 +0100

    Add error page

---
+++ error.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="global-wrapper ErrorPage"
+  >
+    <h1>
+      An Error Occured
+    </h1>
+
+    <p
+      v-if="error.statusCode === 404"
+    >
+      The page you requested cannot be found.
+    </p>
+    <p
+      v-else
+      size="body"
+    >
+      The page you requested cannot be loaded.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorPage',
+
+  props: {
+    error: { type: Object, default: () => {} }
+  }
+}
+</script>
+
+<style lang="postcss">
+.ErrorPage {
+  margin: 40px;
+  background-color: red;
+}
+</style>
